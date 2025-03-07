5875f8fb275a6a850a058aa1da71f0be777d25f2
Henrik Seppänen <henrik.seppanen.a.s@gmail.com>
2025-03-07 12:38:37 +0200

    remove Project folder

--- Project/todo-backend/cron-job/fetch-random-wikipedia.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Fetch a random Wikipedia URL
-RANDOM_URL=$(curl -s -I "https://en.wikipedia.org/wiki/Special:Random" | grep -i "location:" | awk '{print $2}' | tr -d '\r')
-
-# Create a todo with the random URL
-curl -X POST "http://todo-backend-svc:2345/todos" \
-  -H "Content-Type: application/json" \
-  -d "{\"text\": \"Read $RANDOM_URL\"}"
